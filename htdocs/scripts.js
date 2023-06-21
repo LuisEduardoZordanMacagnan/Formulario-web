441df0962bb64bd9135afe665f55b5b03be69cb3
Luís Eduardo <luiseduardozordanmacagnan@gmail.com>
2023-06-21 09:32:10 -0300

    UPDATE FINAL
    Confirmação de senha na troca de senha

--- htdocs/scripts.js
+++ htdocs/scripts.js
@@ -2,10 +2,27 @@ let senha1 = document.getElementById('senha1');
 let senha2 = document.getElementById('senha2');
 let botao = document.getElementById('submit');
 
+let senhaAntiga = document.getElementById('senha').value;
+let campoSenhaAntiga = document.getElementById('campoSenhaAntiga');
+let campoSenhaNova = document.getElementById('campoSenhaNova');
+let submitSenha = document.getElementById('submitSenha');
+
 function compararSenha() {
     if ((senha1.value != senha2.value) || senha1.value == "") {
         botao.disabled = true;
     } else {
         botao.disabled = false;
+    }
+}
+
+function compararSenhaAntiga(){
+    if(campoSenhaNova.value == ""){
+        submitSenha.disabled = false;
+    } else {
+        if(campoSenhaAntiga.value != senhaAntiga){
+            submitSenha.disabled = true;
+        } else {
+            submitSenha.disabled = false;
+        }
     }
 }
--- htdocs/usuarioInfo.php
+++ htdocs/usuarioInfo.php
@@ -20,13 +20,16 @@
     <form method="post" action="atualizarUsuario.php" enctype="multipart/form-data">
         <fieldset class="container">
             <input name="id" type="hidden" value="<?php echo $usuario['id']; ?>" />
+            <input type="hidden" value="<?php echo $usuario['senha']; ?>" id="senha" />
             <legend>Info</legend>
             <label>Nome:</label>
             <input name="nome" type="text" value="<?php echo $usuario['nome']; ?>" />
             <label>Email:</label>
             <input name="email" type="email" value="<?php echo $usuario['email']; ?>" />
-            <label>Senha:</label>
-            <input name="senha" type="password" placeholder="Senha" />
+            <label>Senha antiga:</label>
+            <input type="password" placeholder="Senha" id="campoSenhaAntiga" onkeyup="compararSenhaAntiga()" />
+            <label>Nova senha:</label>
+            <input name="senha" type="password" placeholder="Senha" id="campoSenhaNova" onkeyup="compararSenhaAntiga()" />
             <label>Foto:</label>
             <div>
                 <input style="background-color: rgb(255, 123, 47); color: white" type="file" name="foto" accept="image/png, image/jpeg" />
@@ -35,10 +38,11 @@
             <br />
             <div>
                 <button><a href="home.php?p=">Voltar</a></button>
-                <input style="background-color: rgb(255, 123, 47); color: white" type="submit" value="Atualizar" />
+                <input style="background-color: rgb(255, 123, 47); color: white" type="submit" value="Atualizar" id="submitSenha" />
                 <button><a href="deletarUsuario.php?id=<?php echo $id; ?>">Deletar</a></button>
             </div>
         </fieldset>
     </form>
 </body>
+<script src="scripts.js"></script>
 </html>
